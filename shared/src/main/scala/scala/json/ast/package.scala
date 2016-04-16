d2ae6f30dfdcc012ffa825eb22b0a8bb29cac85f
Matthew de Detrich <mdedetrich@gmail.com>
2016-04-16 08:02:47 +0200

    JNumberRegex has been inlined

--- shared/src/main/scala/scala/json/ast/package.scala
+++ shared/src/main/scala/scala/json/ast/package.scala
@@ -7,7 +7,7 @@ package object ast {
     * precision
     */
 
-  protected[ast] val jNumberRegex: String =
+  @inline protected[ast] val jNumberRegex: String =
     """-?(?:0|[1-9]\d*)(?:\.\d+)?(?:[eE][+-]?\d+)?"""
 
   /**
